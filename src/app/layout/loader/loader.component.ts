8cb466b114b3af9457e83bb4c9523f60f6068ab2
GOKUL-J-DEV <gokul@sparkouttech.com>
2025-05-18 23:40:29 +0530

    feat: add initial loader component and setup

---
+++ src/app/layout/loader/loader.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [CommonModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent implements OnInit, AfterViewInit {
+  hideLoader = false;
+  @ViewChild('loader') loaderElement: ElementRef | undefined;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.hideLoader = true;
+    }, 2000);
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.loaderElement) {
+        this.loaderElement.nativeElement.style.display = 'none';
+      }
+    }, 3000); // 2000ms initial delay + 1000ms second delay
+  }
+}
